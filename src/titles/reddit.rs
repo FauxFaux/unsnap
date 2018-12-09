54974de1ea98c4b7a5457478b57944f1c5f92b9c
Chris West (Faux) <git@goeswhere.com>
2018-12-09 19:03:25 +0000

    v.redd.it: title + dash link

--- src/titles/reddit.rs
+++ src/titles/reddit.rs
@@ -5,12 +5,19 @@ use crate::webs::Webs;
 
 pub fn video<W: Webs>(webs: &W, id: &str) -> Result<String, Error> {
     let base = format!("https://v.redd.it/{}/", id);
+    let html = crate::titles::html::process(webs, &base).ok();
     let mut resp = webs.raw_get(&format!("{}DASHPlaylist.mpd", base))?;
     let mut buf = [0u8; 32 * 1024];
     resp.read_many(&mut buf)?;
     let dash_playlist = String::from_utf8_lossy(&buf);
     Ok(match crate::content::dash::highest_stream(&dash_playlist) {
-        Ok(mp4) => format!("Reddit 'dash' video: {}{}", base, mp4),
-        Err(e) => format!("Reddit video link: {} failed with {}", base, e),
+        Ok(mp4) => match html {
+            Some(title) => format!("{}{} - {}", base, mp4, title),
+            None => format!("Reddit 'dash' link without title: {}{}", base, mp4),
+        },
+        Err(e) => match html {
+            Some(title) => format!("{} [video link failed: {} {}]", title, base, e),
+            None => format!("Reddit 'dash' link mega-fail: {} [{}]", base, e),
+        },
     })
 }
